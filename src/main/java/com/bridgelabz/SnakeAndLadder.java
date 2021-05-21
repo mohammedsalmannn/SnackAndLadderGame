2ce1b57a93453a8a144067d31d628a924d39907a
mohammedsalmannn <rmdsalman001@gmail.com>
2021-05-21 06:40:50 +0530

    Added: Random Value

--- src/main/java/com/bridgelabz/SnakeAndLadder.java
+++ src/main/java/com/bridgelabz/SnakeAndLadder.java
@@ -5,5 +5,7 @@ public class SnakeAndLadder {
         public  int die_rolls=0;
         public static void main(String[] args) {
             System.out.println("position "+ position);
+            int die_rolls=  (int) (Math.floor(Math.random() * 10) % 6 + 1);
+            System.out.println(die_rolls);
         }
 }
