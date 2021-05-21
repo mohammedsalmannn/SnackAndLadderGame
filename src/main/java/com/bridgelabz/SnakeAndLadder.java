84d49c4dd16ddb9ef2f15fbf7b773ae9f0c0a2b3
mohammedsalmannn <rmdsalman001@gmail.com>
2021-05-21 06:33:27 +0530

    Added: Position

---
+++ src/main/java/com/bridgelabz/SnakeAndLadder.java
@@ -0,0 +1,9 @@
+package com.bridgelabz;
+
+public class SnakeAndLadder {
+        public static int position=0;
+        public  int die_rolls=0;
+        public static void main(String[] args) {
+            System.out.println("position "+ position);
+        }
+}
